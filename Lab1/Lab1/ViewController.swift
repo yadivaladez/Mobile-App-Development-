f1a89c2a22510c10e6e18b8e73092647891e6a89
yava9221 <yava9221@colorado.edu>
2015-09-14 14:26:37 -0600

    update

--- Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Lab1
 //
-//  Created by Yadira Valadez on 9/10/15.
+//  Created by Yadira Valadez on 9/11/15.
 //  Copyright (c) 2015 Yadira Valadez. All rights reserved.
 //
 
@@ -10,21 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var mainImage: UIImageView!
-  
-    @IBAction func changePic(sender: UIButton) {
-        if sender.tag == 1{
+    
+    @IBAction func changeImage(sender: UIButton) {
+        if sender.tag == 1 {
+            mainImage.image = UIImage(named: "chicago")
+            
+        }
+        else if sender.tag == 2 {
             mainImage.image = UIImage(named: "leo")
         }
-        else if sender.tag == 2{
-            mainImage.image=UIImage(named: "leopard")
+        else if sender.tag == 3 {
+            mainImage.image = UIImage(named: "leopard")
         }
-        else if sender.tag == 3{
-            mainImage.image=UIImage(named: "chicago")
-        }
-        
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
